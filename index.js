5106bd095474e18e72920b22269d1bde8d20330b
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2024-07-25 23:34:36 -0500

    responsive-cards

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ const main = document.getElementById("features-section");
 const features = [
     {
         name: "Supervisor",
-        paragraph: "Monitors activity to identify project roadblocks"
+        color: "hsl(180, 62%, 55%)",
+        paragraph: "Monitors activity to identify project roadblocks",
+        img: './images/icon-supervisor.svg'
     },
     {
         name: "Team Builder",
-        paragraph: "Scans our talent network to create the optimal team for your project"
+        color: "hsl(0, 78%, 62%)",
+        paragraph: "Scans our talent network to create the optimal team for your project",
+        img: './images/icon-team-builder.svg'
     },
     {
         name: "Karma",
-        paragraph: "Regularly evaluates our talent to ensure quality"
+        color: "hsl(34, 97%, 64%)",
+        paragraph: "Regularly evaluates our talent to ensure quality",
+        img: './images/icon-Karma.svg'
     },
     {
         name: "Calculator",
-        paragraph: "ses data from past projects to provide better delivery estimates"
+        color: "hsl(212, 86%, 64%)",
+        paragraph: "ses data from past projects to provide better delivery estimates",
+        img: './images/icon-calculator.svg'
     }
 ]
 
@@ -25,14 +33,19 @@ createCards();
 function createCards (){
     main.innerHTML = "";
     features.forEach(feature =>{
-        let section = document.createElement('div');
+        let section = document.createElement('section');
         section.classList.add('feature-container');
+        section.style.borderTop = ` 6px solid ${feature.color}` 
         let h2 = document.createElement('h2');
         h2.innerHTML = feature.name ;
         let p = document.createElement('p');
-        p.innerHTML = `${feature.paragraph}` ;
+        p.innerHTML = feature.paragraph ;
+        let img = document.createElement('img');
+        img.src = feature.img;
+        img.alt = `${feature.name}-image`
         section.appendChild(h2);
         section.appendChild(p);
+        section.appendChild(img)
         main.appendChild(section);
     }
     )
